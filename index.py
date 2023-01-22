299add21e6446edc2a491ae98ebb23f6cddb1ddc
mitsu-3s <mitsuuu.s3s@gmail.com>
2023-01-22 11:40:29 +0900

    Create route

--- index.py
+++ index.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
    return render_template('index.html')
+
+@app.route('/next', methods=['GET'])
+def next():
+   return render_template('next.html')
 
 # if __name__ == '__main__':
 #     app.run()
